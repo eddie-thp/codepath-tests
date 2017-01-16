955c3cd35622acf8f2df7bee30cd248e59d5b209
Eduardo Przysiezny <eduardo_thp@yahoo.com.br>
2017-01-15 20:27:06 -0800

    Checkpoint 5.2 - 6th attempt (success)

--- src/Checkpoint5_2.java
+++ src/Checkpoint5_2.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.TreeSet;
 
 /**
@@ -11,7 +12,7 @@ import java.util.TreeSet;
  * The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
  * 
  * Your algorithm should run in O(n) complexity.
- * </pre> 
+ * </pre>
  */
 public class Checkpoint5_2 {
 	public static void main(String[] args) {
@@ -22,10 +23,10 @@ public class Checkpoint5_2 {
 		a.add(1);
 		a.add(3);
 		a.add(2);
-		
+
 		Checkpoint5_2 c = new Checkpoint5_2();
 		System.out.println("Result: " + c.longestConsecutive(a));
-		
+
 		a.clear();
 		a.add(1);
 		a.add(1);
@@ -40,57 +41,42 @@ public class Checkpoint5_2 {
 		System.out.println("Result: " + c.longestConsecutive(a));
 	}
 
+	/**
+	 * If you look at the src control history, there were 6 attempts to solve this exercise
+	 * 2rd and 4th failed due to the same edge case (duplicate numbers in the input)
+	 * 3rd and 5th failed due to efficiency issues
+	 * 6th solves efficiency with the use of Java TreeSet implementation for sorting
+	 *  
+	 * @param a
+	 * @return
+	 */
 	public int longestConsecutive(final List<Integer> a) {
 
-        int longest = 0;
-        List<int[]> sequences = new ArrayList<int[]>();
-	    for (int i = 0; i < a.size(); i++) {
-            int n = a.get(i);	        
-            
-            int[] sequenceRange = new int[2];
-            sequenceRange[0] = n;
-            sequenceRange[1] = n;
+		Set<Integer> sa = new TreeSet<Integer>(a);
 
-            boolean inserted = false;
-            if (sequences.size() > 0)
-            {
-                for (int[] existingSequence : sequences)
-                {
-                    int first = existingSequence[0];
-                    int last = existingSequence[1];
-                    
-                    if (n == first || n == last) {
-                        // Ignore number that has already been processed
-                        sequenceRange = existingSequence;
-                        inserted = true;
-                        break;
-                    }
+		int longestSize = 1;
 
-                    // Is n before 1st ?
-                    if (n + 1 == first) {
-                        existingSequence[0] = sequenceRange[0];
-                        sequenceRange = existingSequence;
-                        inserted = true;
-                    } 
-                    // Is n after last ?
-                    else if (n - 1 == last) {
-                        existingSequence[1] = sequenceRange[1];
-                        sequenceRange = existingSequence;
-                        inserted = true;
-                    }
-                }
-            }
-            
-            if (!inserted) {
-                sequences.add(sequenceRange);
-            }
-            
-            int sRangeSize = (sequenceRange[1] - sequenceRange[0]) + 1;
-            if (sRangeSize > longest) {
-                longest = sRangeSize;
-            }
-	    }
-	    
-	    return longest;
+		int previousN = 0;
+		int currentSize = 0;
+		int count = 0;
+		for (Integer n : sa) {
+
+			if (count > 0 && n != previousN + 1) {
+				// Sequence has changed
+				if (currentSize > longestSize) {
+					longestSize = currentSize;
+				}
+				currentSize = 0;
+			}
+			previousN = n;
+			++currentSize;
+			++count;
+		}
+
+		if (currentSize > longestSize) {
+			longestSize = currentSize;
+		}
+
+		return longestSize;
 	}
 }
