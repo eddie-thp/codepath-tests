b8707e9a38022a998a3082074225385da7489ac6
Eduardo Przysiezny <eduardo_thp@yahoo.com.br>
2017-01-26 00:30:26 -0800

    TwoSum - fix edge case fix of efficient solution

--- src/week2/hash/TwoSum.java
+++ src/week2/hash/TwoSum.java
@@ -28,7 +28,6 @@ public class TwoSum {
 		a.add(7);
 		a.add(11);
 		a.add(15);
-		
 		System.out.println("INDEXES: " + c.twoSum(a, 9));
 
 		List b = new ArrayList<Integer>();
@@ -69,19 +68,68 @@ public class TwoSum {
 		d.add(1);
 		d.add(1);
 		d.add(1);
-		System.out.println("INDEXES: " + c.twoSum(d, -3));
+		System.out.println("INDEXES: " + c.twoSum(d, 2));
+		
+		List e = new ArrayList<Integer>();
+		e.add(0);
+		e.add(8);
+		e.add(-3);
+		e.add(-1);
+		e.add(7);
+		e.add(9);
+		e.add(-1);
+		e.add(8);
+		e.add(-2);
+		e.add(2);
+		e.add(-8);
+		e.add(-6);
+		e.add(-7);
+		e.add(-4);
+		e.add(-6);
+		e.add(-1);
+		e.add(-6);
+		e.add(6);
+		e.add(8);
+		e.add(-10);
+		e.add(-6);
+		e.add(4);
+		e.add(-8);
+		e.add(7);
+		e.add(6);
+		e.add(-4);
+		e.add(-4);
+		e.add(-10);
+		e.add(-6);
+		e.add(5);
+		e.add(-8);
+		e.add(-1);
+		e.add(10);
+		e.add(6);
+		e.add(6);
+		e.add(-3);
+		e.add(-3);
+		e.add(-7);
+		e.add(-8);
+		e.add(-7);
+		e.add(4);
+		e.add(-7);
+		e.add(1);
+		e.add(-10);
+		e.add(5);
+		System.out.println("INDEXES: " + c.twoSum(e, 2));
 	}
 
 	public ArrayList<Integer> twoSum(final List<Integer> a, int b) {
 	    ArrayList<Integer> operands = new ArrayList<Integer>();
 
         // Put array values into map
-	    HashMap<Integer, Integer> aMap = new HashMap<Integer, Integer>();
+	    HashMap<Integer, List<Integer>> aMap = new HashMap<Integer, List<Integer>>();
         for (int i = 0; i < a.size(); i++) {
-            int n2 = a.get(i);
-            if (!aMap.containsKey(n2)) {
-                aMap.put(n2, i);        
+            int n = a.get(i);
+            if (!aMap.containsKey(n)) {
+                aMap.put(n, new ArrayList<Integer>());
             }
+            aMap.get(n).add(i);
         }
 
 	    for (int i = 0; i < a.size() ; i++) {
@@ -89,17 +137,22 @@ public class TwoSum {
 	        int n2 = b - n1;
 
             if (aMap.containsKey(n2)) {
-                int j = aMap.get(n2);
+                int index = (n2 == n1 ? 1 : 0);
                 
-                if (j > i) {
-                    if (operands.isEmpty()) {
-                        operands.add(i + 1);
-                        operands.add(j + 1);
-	                } else if (operands.get(1) - 1 > j) {
-	                    operands.set(0, i + 1);
-	                    operands.set(1, j + 1);
-	                }
-	            }
+                List<Integer> indexes = aMap.get(n2);
+                if (index < indexes.size()) {
+	                int j = indexes.get(index);
+	                
+	                if (j > i) {
+	                    if (operands.isEmpty()) {
+	                        operands.add(i + 1);
+	                        operands.add(j + 1);
+		                } else if (operands.get(1) - 1 > j) {
+		                    operands.set(0, i + 1);
+		                    operands.set(1, j + 1);
+		                }
+		            }
+                }
 	        }
 	    }
 	    
