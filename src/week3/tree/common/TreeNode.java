f80336e935e701cbb32d195f2e6833d6e9005eca
Eduardo Przysiezny <eduardo_thp@yahoo.com.br>
2017-02-05 18:59:52 -0800

    TreeNode factory method to create nodes from array

--- src/week3/tree/common/TreeNode.java
+++ src/week3/tree/common/TreeNode.java
@@ -1,5 +1,8 @@
 package week3.tree.common;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Definition for binary tree
  */
@@ -10,5 +13,43 @@ public class TreeNode {
 
 	public TreeNode(int x) {
 		val = x;
+	}
+	
+	public static TreeNode fromArray(int[] nodeValues) {
+		TreeNode root = null;
+		
+		if (nodeValues.length > 0){
+			List<TreeNode> nodes = new ArrayList<TreeNode>();
+			
+			for (int i = 0; i < nodeValues.length; i++) {
+				TreeNode node = null;
+				
+				int nodeVal = nodeValues[i];
+				if (nodeVal != -1) {
+					node = new TreeNode(nodeVal);
+					
+					if (i > 0) {
+						int parent = i / 2;
+						
+						boolean left = (i%2) > 0;
+
+						if (left) {
+							TreeNode parentNode = nodes.get(parent);
+							parentNode.left = node;
+						} else {
+							parent -= 1;
+							TreeNode parentNode = nodes.get(parent);
+							parentNode.right = node;
+						}
+					}
+				}
+				
+				nodes.add(node);
+			}
+			
+			root = nodes.get(0);
+		}
+		
+		return root;
 	}
 }
